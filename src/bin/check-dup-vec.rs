45588c968989276e492fbb5ac9b653f53c760d7b
Tatsuya Teshima <terima666tatsuya@gmail.com>
2025-02-01 18:05:57 +0900

    add has_duplicates

--- src/bin/check-dup-vec.rs
+++ src/bin/check-dup-vec.rs
@@ -1,11 +1,15 @@
 use std::collections::HashSet;
+use std::hash::Hash;
+
+fn has_duplicates<T: Eq + Hash>(input: &[T]) -> bool {
+    let hs: HashSet<_> = input.iter().collect();
+    hs.len() != input.len()
+}
 
 fn main() {
     let input = vec!["r", "u", "s", "t", "r"];
 
-    let hs: HashSet<_> = input.iter().collect();
-
-    if hs.len() != input.len() {
+    if has_duplicates(&input) {
         println!("重複あり");
     } else {
         println!("重複なし")
